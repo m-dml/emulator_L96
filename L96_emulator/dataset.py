1d19d53cb0a53e2ac0f922b73ecab67b37728307
mnonnenm <mnonnenm@uos.de>
2020-07-13 14:23:32 +0200

    cleaning up different versions of the dataset class

--- L96_emulator/dataset.py
+++ L96_emulator/dataset.py
@@ -1,61 +1,5 @@
 import torch
 import numpy as np
-
-class Dataset(torch.utils.data.IterableDataset):
-    def __init__(self, data, offset=1, J=0,
-                 start=None, end=None, 
-                 normalize=False, randomize_order=True):
-
-        if len(data.shape) == 2:
-            self.J, self.K = J, data.shape[1]//(J+1)
-            assert data.shape[1]/(J+1) == self.K
-            self.data = data.copy().reshape(-1, self.J+1, self.K)
-
-        self.offset = offset
-        if start is None or end is None:
-            start, end = 0,  self.data.shape[0]-self.offset
-        assert end > start
-        self.start, self.end = start, end
-
-        self.normalize = normalize
-        self.mean, self.std = 0., 1.
-        if self.normalize:
-            self.mean = self.data.mean(axis=(0,2)).reshape(1,-1,1)
-            self.std = self.data.std(axis=(0,2)).reshape(1,-1,1)
-            self.data = (self.data - self.mean) / self.std 
-
-        self.randomize_order = randomize_order
-
-    def __getitem__(self, index):
-        """ Generate one batch of data """
-        idx = np.atleast_1d(np.asarray(index))
-        return self.data[idx]
-
-    def __iter__(self):
-        """ Return iterable over data in random order """
-        iter_start, iter_end = self.divide_workers()
-        if self.randomize_order:
-            idx = torch.randperm(iter_end - iter_start, device='cpu') + iter_start
-        else: 
-            idx = torch.arange(iter_start, iter_end, requires_grad=False, device='cpu')
-
-        X = self.data[idx,:]
-        y = self.data[idx+self.offset,:]
-
-        return zip(X, y)
-
-    def __len__(self):
-        return (self.end - self.start) #self.data.shape[0]
-
-    def divide_workers(self):
-        """ parallelized data loading via torch.util.data.Dataloader """
-        if torch.utils.data.get_worker_info() is None:
-            iter_start = torch.tensor(self.start, requires_grad=False, dtype=torch.int, device='cpu')
-            iter_end = torch.tensor(self.end, requires_grad=False, dtype=torch.int, device='cpu')
-        else: 
-            raise NotImplementedError('had no need for parallelization yet')
-
-        return iter_start, iter_end
    
 class Dataset(torch.utils.data.IterableDataset):
     def __init__(self, data, offset=1, J=0,
@@ -113,64 +57,8 @@ class Dataset(torch.utils.data.IterableDataset):
 
         return iter_start, iter_end
 
-class Dataset(torch.utils.data.IterableDataset):
-    def __init__(self, data, offset=1, J=0,
-                 start=None, end=None, 
-                 normalize=False, randomize_order=True):
 
-        if len(data.shape) == 2:
-            self.J, self.K = J, data.shape[1]//(J+1)
-            assert data.shape[1]/(J+1) == self.K
-            self.data = data.copy().reshape(-1, self.J+1, self.K)
-
-        self.offset = offset
-        if start is None or end is None:
-            start, end = 0,  self.data.shape[0]-self.offset
-        assert end > start
-        self.start, self.end = start, end
-
-        self.normalize = normalize
-        self.mean, self.std = 0., 1.
-        if self.normalize:
-            self.mean = self.data.mean(axis=(0,2)).reshape(1,-1,1)
-            self.std = self.data.std(axis=(0,2)).reshape(1,-1,1)
-            self.data = (self.data - self.mean) / self.std 
-
-        self.randomize_order = randomize_order
-
-    def __getitem__(self, index):
-        """ Generate one batch of data """
-        idx = np.atleast_1d(np.asarray(index))
-        return self.data[idx]
-
-    def __iter__(self):
-        """ Return iterable over data in random order """
-        iter_start, iter_end = self.divide_workers()
-        if self.randomize_order:
-            idx = torch.randperm(iter_end - iter_start, device='cpu') + iter_start
-        else: 
-            idx = torch.arange(iter_start, iter_end, requires_grad=False, device='cpu')
-
-        X = self.data[idx,:]
-        y = self.data[idx+self.offset,:]
-
-        return zip(X, y)
-
-    def __len__(self):
-        return (self.end - self.start) #self.data.shape[0]
-
-    def divide_workers(self):
-        """ parallelized data loading via torch.util.data.Dataloader """
-        if torch.utils.data.get_worker_info() is None:
-            iter_start = torch.tensor(self.start, requires_grad=False, dtype=torch.int, device='cpu')
-            iter_end = torch.tensor(self.end, requires_grad=False, dtype=torch.int, device='cpu')
-        else: 
-            raise NotImplementedError('had no need for parallelization yet')
-
-        return iter_start, iter_end
-
-
-class DatasetRelPred(torch.utils.data.IterableDataset):
+class DatasetRelPred(Dataset):
     def __init__(self, data, offset=1, J=0,
                  start=None, end=None, 
                  normalize=False, randomize_order=True):
@@ -211,20 +99,22 @@ class DatasetRelPred(torch.utils.data.IterableDataset):
         else: 
             idx = torch.arange(iter_start, iter_end, requires_grad=False, device='cpu')
 
-        X = np.concatenate((self.data[idx,:], self.data[idx,:]-self.data[idx-self.offset,:]), axis=1)
+        X = self.data[idx,:]
         y = (self.data[idx+self.offset,:] - self.data[idx,:] - self.mean_out) / self.std_out
 
         return zip(X, y)
 
-    def __len__(self):
-        return (self.end - self.start) #self.data.shape[0]
+class DatasetRelPredPast(DatasetRelPred):
 
-    def divide_workers(self):
-        """ parallelized data loading via torch.util.data.Dataloader """
-        if torch.utils.data.get_worker_info() is None:
-            iter_start = torch.tensor(self.start, requires_grad=False, dtype=torch.int, device='cpu')
-            iter_end = torch.tensor(self.end, requires_grad=False, dtype=torch.int, device='cpu')
+    def __iter__(self):
+        """ Return iterable over data in random order """
+        iter_start, iter_end = self.divide_workers()
+        if self.randomize_order:
+            idx = torch.randperm(iter_end - iter_start, device='cpu') + iter_start
         else: 
-            raise NotImplementedError('had no need for parallelization yet')
+            idx = torch.arange(iter_start, iter_end, requires_grad=False, device='cpu')
 
-        return iter_start, iter_end
+        X = np.concatenate((self.data[idx,:], self.data[idx,:]-self.data[idx-self.offset,:]), axis=1)
+        y = (self.data[idx+self.offset,:] - self.data[idx,:] - self.mean_out) / self.std_out
+
+        return zip(X, y)
