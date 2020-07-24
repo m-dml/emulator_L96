535f2baee69f938c2c0167549c28f003a76c88cc
mnonnenm <mnonnenm@uos.de>
2020-07-24 15:40:20 +0200

    fixing serious issue with predictor-corrector method - so far was overwriting f(t,x(t)) with f(t,x(t)+dt*f(t,x(t))),   i.e. effectively was operating with alpha=0 all the time.

--- L96_emulator/util.py
+++ L96_emulator/util.py
@@ -39,14 +39,14 @@ def sortL96fromChannels(x):
 
 def predictor_corrector(fun, y0, times, alpha=0.5):
 
-    y = np.zeros((len(times), *y0.shape))
+    y = np.zeros((len(times), *y0.shape), dtype=y0.dtype)
     y[0] = y0
     for i in range(1,len(times)):        
         dt = times[i] - times[i-1]
 
-        f0 = fun(times[i-1], y[i-1])
+        f0 = fun(times[i-1], y[i-1]).copy()
         f1 = fun(times[i],   y[i-1] + dt*f0)
 
-        y[i] = y[i-1] + dt * (alpha*f0 + (1-alpha)*f1)
+        y[i] = y[i-1] + dt * (alpha*f0 + (1.-alpha)*f1)
         
     return y
