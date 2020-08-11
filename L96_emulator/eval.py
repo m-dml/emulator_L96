739da71a3f5a27b3594939477c86438e7dd01b56
mnonnenm <mnonnenm@uos.de>
2020-08-11 21:45:21 +0200

    cleaning up eval.py, removing superfluous solve_from_init function - can do the same with either of the numpy solvers (pred-corr or rk4)

--- L96_emulator/eval.py
+++ L96_emulator/eval.py
@@ -35,26 +35,6 @@ class Rollout(torch.nn.Module):
             raise NotImplementedError
 
 
-def solve_from_init(K, J, T_burnin, T_, dt, F, h, b, c, data, dilation=2, norm_mean=0., norm_std=1.):
-
-    assert int(dilation) == dilation
-
-    dX_dt = np.empty(K*(J+1), dtype=data.dtype)
-    times = np.arange(0, (T_+1)*dt, dt/dilation)
-
-    if J > 0:
-        def fun(t, x):
-            return f2(x, F, h, b, c, dX_dt, K, J)
-    else:
-        def fun(t, x):
-            return f1(x, F, dX_dt, K)
-
-    out2 = predictor_corrector(fun=fun, y0=data[T_burnin], times=times, alpha=0.5)[::dilation,:]
-    out2 = sortL96fromChannels((sortL96intoChannels(out2,J) - norm_mean) / norm_std)
-
-    return out2
-
-
 def load_model_from_exp_conf(res_dir, conf):
     
     net_kwargs = {
@@ -71,7 +51,8 @@ def load_model_from_exp_conf(res_dir, conf):
             'K_net' : conf['K_net'], 
             'J_net' : conf['J_net'], 
             'dt_net' : conf['dt_net'], 
-            'alpha_net' : conf['alpha_net']            
+            'alpha_net' : conf['alpha_net'],
+            'model_forwarder' : conf['model_forwarder']
     }
 
     model, model_forward = named_network(model_name=conf['model_name'], 
@@ -112,12 +93,12 @@ def get_rollout_fun(dg_train, model_forward, prediction_task):
         std_out = torch.as_tensor(dg_train.std_out, device=device, dtype=dtype)
         mean_out = torch.as_tensor(dg_train.mean_out, device=device, dtype=dtype)
 
-        def model_simulate(y0, dy0, T):
-            x = np.empty((T+1, *y0.shape[1:]))
+        def model_simulate(y0, dy0, n_steps):
+            x = np.empty((n_steps+1, *y0.shape[1:]))
             x[0] = y0.copy()
             xx = torch.as_tensor(x[0], device=device, dtype=dtype)
             dx = torch.as_tensor(dy0.copy(), device=device, dtype=dtype)
-            for i in range(1,T+1):
+            for i in range(1,n_steps+1):
                 xxo = xx * 1.
                 xx = std_out * model_forward(torch.cat((xx.reshape(1,J+1,K), dx), axis=1)) + mean_out + xx.reshape(1,J+1,-1)
                 dx = xx - xxo
@@ -129,11 +110,11 @@ def get_rollout_fun(dg_train, model_forward, prediction_task):
         raise NotImplementedError
 
         assert isinstance(dg_train, DatasetRelPred)
-        def model_simulate(y0, dy0, T):
-            x = np.empty((T+1, *y0.shape[1:]))
+        def model_simulate(y0, dy0, n_steps):
+            x = np.empty((n_steps+1, *y0.shape[1:]))
             x[0] = y0.copy()
             xx = torch.as_tensor(x[0], device=device, dtype=dtype).reshape(1,1,-1)
-            for i in range(1,T+1):
+            for i in range(1,n_steps+1):
                 xx = model_forward(xx.reshape(1,J+1,-1)) + xx.reshape(1,J+1,-1)
                 x[i] = xx.detach().cpu().numpy().copy()
             return x
@@ -141,11 +122,11 @@ def get_rollout_fun(dg_train, model_forward, prediction_task):
     elif prediction_task == 'state': 
 
         assert isinstance(dg_train, Dataset)
-        def model_simulate(y0, dy0, T):
-            x = np.empty((T+1, *y0.shape[1:]))
+        def model_simulate(y0, dy0, n_steps):
+            x = np.empty((n_steps+1, *y0.shape[1:]))
             x[0] = y0.copy()
             xx = torch.as_tensor(x[0], device=device, dtype=dtype).reshape(1,1,-1)
-            for i in range(1,T+1):
+            for i in range(1,n_steps+1):
                 xx = model_forward(xx.reshape(1,J+1,-1))
                 x[i] = xx.detach().cpu().numpy().copy()
             return x
@@ -153,18 +134,18 @@ def get_rollout_fun(dg_train, model_forward, prediction_task):
     return model_simulate
 
 
-def plot_rollout(out, out_model, out_comparison, T_start, T_dur, K=None, fig=None):
+def plot_rollout(out, out_model, out_comparison, n_start, n_steps, K=None, fig=None):
 
-    vmax = np.maximum(np.nanmax(out[np.arange(T_dur)+T_start]),
+    vmax = np.maximum(np.nanmax(out[np.arange(n_steps)+n_start]),
                       np.nanmax(out_model))
-    vmin = np.minimum(np.nanmin(out[np.arange(T_dur)+T_start]),
+    vmin = np.minimum(np.nanmin(out[np.arange(n_steps)+n_start]),
                       np.nanmin(out_model.T))
 
     fig = plt.figure(figsize=(16,9)) if fig is None else fig
     
     plt.figure(fig.number)
     plt.subplot(2,2,1)
-    plt.imshow(out[np.arange(T_dur+1)+ T_start].T, aspect='auto', vmin=vmin, vmax=vmax)
+    plt.imshow(out[np.arange(n_steps+1)+ n_start].T, aspect='auto', vmin=vmin, vmax=vmax)
     plt.xlabel('time')
     plt.ylabel('location')
     plt.title('numerical simulation')
@@ -177,22 +158,23 @@ def plot_rollout(out, out_model, out_comparison, T_start, T_dur, K=None, fig=Non
     plt.colorbar()
 
     plt.subplot(1,2,2)
-    plt.plot(np.sqrt(np.mean( (out_model - out[np.arange(T_dur+1)+ T_start])**2, axis=1 )), 
+    plt.plot(np.mean( (out_model - out[np.arange(n_steps+1)+ n_start])**2, axis=1 ), 
              'b', label='model-reconstruction vs sim')
+    KJ1 = out_model.shape[1]
     if not K is None:
-        plt.plot(np.sqrt(np.mean( (out_model[:,:K] - out[np.arange(T_dur+1)+ T_start][:,:K])**2, axis=1 )), 
-                 'b--', label='model-reconstruction vs sim, slow vars only')
+        plt.plot(np.sum( (out_model[:,:K] - out[np.arange(n_steps+1)+ n_start][:,:K])**2, axis=1 )/KJ1, 
+                 'b--', label='model-reconstruction vs sim, fraction from slow vars only')
     try:
-        plt.plot(np.sqrt(np.mean( (out_comparison[:T_dur+1] - out[np.arange(T_dur+1)+ T_start])**2, axis=1 )),
+        plt.plot(np.mean( (out_comparison[:n_steps+1] - out[np.arange(n_steps+1)+ n_start])**2, axis=1 ),
                 'k', label='solver 2x temp. resol. vs sim')
         if not K is None:
-            plt.plot(np.sqrt(np.mean( (out_comparison[:T_dur+1][:,:K] - out[np.arange(T_dur+1)+ T_start][:,:K])**2, axis=1 )),
-                    'k--', label='solver 2x temp. resol. vs sim, slow vars only')
+            plt.plot(np.sum( (out_comparison[:n_steps+1][:,:K] - out[np.arange(n_steps+1)+ n_start][:,:K])**2, axis=1 )/KJ1,
+                    'k--', label='solver 2x temp. resol. vs sim, fraction from slow vars only')
     except:
         pass
     plt.title('missmatch over time')
-    plt.xlabel('time')
-    plt.ylabel('RMSE (on z-scored data)')
+    plt.xlabel('time (# integration steps)')
+    plt.ylabel('MSE')
     plt.legend()
     #plt.show()
 
