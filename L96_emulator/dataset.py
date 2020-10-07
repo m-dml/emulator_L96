02cc41d5674b55f36bbdc81d3b723460a1c7a87b
mnonnenm <mnonnenm@uos.de>
2020-10-07 17:55:04 +0200

    adding new dataset class which subdivides the L96 ring

--- L96_emulator/dataset.py
+++ L96_emulator/dataset.py
@@ -72,7 +72,7 @@ class DatasetMultiTrial(Dataset):
 
         self.offset = offset
         if start is None or end is None:
-            start, end = 0,  self.data.shape[0]-self.offset
+            start, end = 0,  self.T-self.offset
         assert end > start
         self.start, self.end = start, end
 
@@ -106,7 +106,58 @@ class DatasetMultiTrial(Dataset):
         return zip(X, y)
 
     def __len__(self):
-        return self.N * (self.end - self.start) #self.data.shape[0]
+        return self.N * (self.end - self.start)
+
+
+class DatasetMultiTrial_shattered(DatasetMultiTrial):
+    def __init__(self, data, offset=1, J=0,
+                 start=None, end=None, K_local=None,
+                 normalize=False, randomize_order=True):
+
+        super(DatasetMultiTrial_shattered, self).__init__(
+                 data=data, offset=offset, J=J, start=start, end=end, 
+                 normalize=normalize, randomize_order=randomize_order
+        )
+        self.K_local = self.K if K_local is None else K_local
+        assert self.K_local <= self.K
+
+        idx_Ks_out, idx_Ks_in =[], []
+        for k in np.arange(0, self.K-self.K_local+1, self.K_local):
+            idx_Ks_in.append(np.mod(np.arange(-2,self.K_local+1)+k, self.K))
+            idx_Ks_out.append(np.arange(self.K_local)+k)
+
+        self.l_regs = len(idx_Ks_in)
+        self.idx_Ks_in, self.idx_Ks_out = np.concatenate(idx_Ks_in), np.concatenate(idx_Ks_out)
+                             
+
+    def __getitem__(self, index):
+        """ Generate one batch of data """
+        idx = np.atleast_1d(np.asarray(index))
+        return self.data[idx]
+
+    def __iter__(self):
+        """ Return iterable over data in random order """
+        iter_start, iter_end = self.divide_workers()
+        if self.randomize_order:
+            idx = [torch.randperm(iter_end - iter_start, device='cpu') for j in range(self.N)]
+            idx = torch.cat([j*self.T + iter_start + idx[j] for j in range(len(idx))])
+        else:
+            idx = [torch.arange(iter_start, iter_end, requires_grad=False, device='cpu') for j in range(self.N)]
+            idx = torch.cat([j*self.T + idx[j] for j in range(len(idx))])
+
+        X = self.data[:,:,self.idx_Ks_in][idx].reshape(-1,self.J+1,self.l_regs,self.K_local+3)
+        y = self.data[:,:,self.idx_Ks_out][idx+self.offset].reshape(-1,self.J+1,self.l_regs,self.K_local)
+
+        X = X.transpose(0,2,1,3)
+        y = y.transpose(0,2,1,3)
+
+        X = X.reshape(-1, *X.shape[2:])
+        y = y.reshape(-1, *y.shape[2:])
+
+        return zip(X, y)
+
+    def __len__(self):
+        return self.l_regs * self.N * (self.end - self.start)
 
 
 class DatasetRelPred(Dataset):
